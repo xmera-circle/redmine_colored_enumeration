e5e422a27d9114a0ed2b0b9ffb0c3d1eb8a6dcdb
Liane Hampe <liaham@xmera.de>
2021-07-25 13:43:44 +0200

    Initial commit
    Initially commit the plugin skeleton with propably all required but empty
    files.

---
+++ db/migrate/001_add_color_column_to_custom_field_enumeration.rb
@@ -0,0 +1,5 @@
+class AddColorColumnToCustomFieldEnumeration < ActiveRecord::Migration[5.2]
+  def change
+    #
+  end
+end
---
+++ init.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Redmine plugin called Redmine Colored Enumeration.
+#
+# Copyright (C) 2021 Liane Hampe <liaham@xmera.de>, xmera.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+require 'colored_enumeration'
+
+Redmine::Plugin.register :redmine_colored_enumeration do
+  name 'Colored Enumeration'
+  author 'Liane Hampe'
+  description 'Background color for custom field enumeration values'
+  version '0.1.0'
+  url 'https://circle.xmera.de/projects/redmine-colored-enumeration'
+  author_url 'http://xmera.de'
+
+  requires_redmine version_or_higher: '4.2.1'
+end
